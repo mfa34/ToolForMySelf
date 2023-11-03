04485de35522054ba61501366cb860a526491962
mfa34 <mfa34@protonmail.com>
2023-11-03 15:09:15 +0300

    added python-OCR tool for my self

---
+++ PythonOCR.py
@@ -0,0 +1,32 @@
+from PIL import Image
+import pytesseract
+from docx import Document
+
+# Başlangıç ve bitiş sayfalarını giriniz
+start_page = 1
+end_page = 128
+
+# İşlem başladı uyarısı
+print("İşlem başladı...")
+
+# Klasördeki her resmin  işlenmesi
+for page_number in range(start_page, end_page + 1):
+    # Resmin adını oluşturun
+    image_file = f'D:/AILE/FATIH/CAHITBULUTABI/Iman_ve_Islam_Arapca/ORCDeneme/٦- الايمان والإسلام_page-{page_number:04d}.jpg'
+
+    # Word belgesi oluşturuluyor
+    doc = Document()
+
+    # Görüntüdeki metni tanıyor
+    image = Image.open(image_file)
+    text = pytesseract.image_to_string(image, lang='ara')  # Hintce icin = hin degerini kullaniniz.
+    doc.add_paragraph(text)
+
+    # DOCX belgesini kaydediyor
+    output_file = f'D:/AILE/FATIH/CAHITBULUTABI/Iman_ve_Islam_Arapca/ORCDeneme/٦- الايمان والإسلام_page-{page_number:04d}.docx'
+    doc.save(output_file)
+
+    print(f"Sayfa {page_number} tamamlandı. {output_file} dosyası oluşturuldu.")
+
+# İşlem tamamlandı uyarısı
+print("İşlem tamamlandı. Tüm sayfalar dönüştürüldü.")
